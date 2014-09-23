c443d15d3f178c91f8352a8e765cb8567e1ce973
popeye-lxw <lxw9586@live.com>
2014-09-23 11:50:53 +0800

    第一版

---
+++ ConsoleApiTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApiTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //本工程用来测试各种网络接口的请求及相应信息
+            //支持cookies请求
+        }
+    }
+}
